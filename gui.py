7b20e17b925205eb7cc82cad57975795c8bf93ff
Tortole <xorid@mail.ru>
2022-09-27 12:51:08 +0800

    swith on pysimplegui

--- gui.py
+++ gui.py
@@ -1,71 +1,60 @@
-# from kivy.uix.button import Button
-# from kivy.app import App
-# from functools import partial
-#
-#
-# class KivyButton(App):
-#     def disable(self, instance, *args):
-#         instance.disabled = True
-#
-#     def update(self, instance, *args):
-#         instance.text = "I am Disabled!"
-#
-#     def build(self):
-#         mybtn = Button(text="Click me to disable")
-#         mybtn.bind(on_press=partial(self.disable, mybtn))
-#         mybtn.bind(on_press=partial(self.update, mybtn))
-#         return mybtn
-#
-#
-# KivyButton().run()
+import PySimpleGUI as sg
 
+# help(sg.Column)
 
-from kivy.app import App
-from kivy.lang import Builder
+test_list = list(range(20))
 
-kv = '''
-FloatLayout:
-    # Define the root widget
-    ScatterLayout:
-        size_hint: 1.0, 0.2
-        do_translation_x: False
-        # do_translation_y: False
-        Label:
-            size_hint: 1.0, 1
-            text: 'Drag me'
-            canvas.before:
-                Color:
-                    rgb: .6, .6, .6
-                Rectangle:
-                    pos: self.pos
-                    size: self.size
-'''
+def up_elem(button_key):
+    pass
 
-class RectangleApp(App):
-    def build(self):
-        object = Builder.load_string(kv)
-        return object
+column1 = [
+    [sg.Button(f'up', key=f'up-{i}'), sg.Text(f'Scrollable {v}')] for i, v in enumerate(test_list) 
+]
 
-if __name__ == '__main__':
-    RectangleApp().run()
+# column2 = [
+#     [sg.Text(f'Static{i}')] for i in range(10) 
+# ]
+
+layout = [
+    [
+        sg.Column(column1, scrollable=True,  vertical_scroll_only=True, size=(1500, 600)),
+        # sg.Column(column2)
+    ]
+]
+
+window = sg.Window(
+    'Scrollable',
+    layout,
+    # size=(2000, 1200)
+)
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+
+window.close()
 
 
 
-# from kivy.app import App
-# from kivy.uix.widget import Widget
-# from kivy.uix.label import Label
-#
-# class MyWidget(Widget):
-#     def draw(self):
-#         for i in range(6):
-#             print('I draw label')
-#             self.add_widget(Label(text='hello', pos=(100, i*100)))
-#
-# class MyApp(App):
-#     def build(self):
-#         game = MyWidget()
-#         game.draw()
-#         return game
-#
-# if __name__ == '__main__':
-#     MyApp().run()
+
+# import PySimpleGUI as sg
+
+# sg.theme('DarkAmber')   # Add a touch of color
+# # All the stuff inside your window.
+# layout = [  [sg.Text('Some text on Row 1')],
+#             [sg.Text('Enter something on Row 2'), sg.InputText()],
+#             [sg.Button('Ok'), sg.Button('Cancel')] ]
+
+# # Create the Window
+# window = sg.Window('Window Title', layout)
+# # Event Loop to process "events" and get the "values" of the inputs
+# while True:
+#     event, values = window.read()
+#     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+#         break
+#     print('You entered ', values[0])
+
+# window.close()
