28d24424c5376d3a0f7fc57038483a25ea0f094b
Tortole <xorid@mail.ru>
2022-10-01 14:36:41 +0800

    add string convertation in actionstrack class

--- main.py
+++ main.py
@@ -1,248 +1,7 @@
-import time
-from tracemalloc import is_tracing
-from pynput import keyboard, mouse
-from screeninfo import get_monitors
-
-class ActionsTrack:
-    mouse_buttom_to_string = {
-        mouse.Button.left: 'l',
-        mouse.Button.right: 'r',
-        mouse.Button.middle: 'm'
-    }
-    string_to_mouse_button = {v: k for k, v in mouse_buttom_to_string.items()}
-    
-    _space_key_code = '000'
-
-    def __init__(self):
-        # Запущена ли прослушка клавиатуры и мыши
-        self.is_listening = False
-        self.is_tracking = False
-        # Прослушка клавиатуры
-        self.keyboard_listener = None
-        # Прослушка мыши
-        self.mouse_listener = None
-        # Клавиша для запуска или остановки макроса
-        self.hotkey_macros_write = keyboard.Key.shift_r
-        # Значения разрешения главного экрана
-        self.primary_monitor_resolution = {}
-        # Получение данных о разрешении монитора
-        for m in get_monitors():
-            if m.is_primary:
-                primary_monitor_resolution['width'] = m.width
-                primary_monitor_resolution['height'] = m.height
-                break
-        # Последовательность действий клавиатуры и мыши
-        self.track = []
-
-    def _add_action(self, device, action, **kwargs):
-        if action not in ['press', 'release', 'move', 'scroll']:
-            raise ValueError('Wrong action name.')
-
-        if device == 'keyboard':
-            self.track.append({
-                'device': device,
-                'action': action,
-                'key': kwargs['key']
-            })
-        elif device == 'mouse':
-            action_dict = {
-                'device': device,
-                'action': action,
-                'x': kwargs['x'],
-                'y': kwargs['y']
-            }
-
-            if action == 'press' or action == 'release':
-                action_dict['key'] = kwargs['key']
-            if action == 'scroll':
-                action_dict['dx'] = kwargs['dx']
-                action_dict['dy'] = kwargs['dy']
-
-            self.track.append(action_dict)
-        else:
-            raise ValueError('Wrong device name.')
-
-    # vvvv mouse vvvv
-
-    # def coord_to_string(x, y):
-    #     return f'{x / primary_monitor_resolution["width"]:.2f}{y / primary_monitor_resolution["height"]:.2f}'
-
-
-    # def string_to_coord(str_coord):
-    #     return round(float(str_coord[:4]) * primary_monitor_resolution['width']), \
-    #         round(float(str_coord[4:]) * primary_monitor_resolution['height'])
-
-    def on_move_mouse_track(self, x, y):
-        if self.is_tracking:
-            pass
-
-    def on_click_mouse_track(self, x, y, button, is_pressed):
-        # global is_listen_run
-        # if is_listen_run:
-        #     track_actions.append(
-        #         f'm'
-        #         f'{coord_to_string(x, y)}'
-        #         f'{"p" if is_pressed else "r"}'
-        #         f'{mouse_buttom_to_string[button]}'
-        #     )
-        if self.is_tracking:
-            self._add_action(
-                'mouse',
-                'press' if is_pressed else 'release',
-                x=x,
-                y=y,
-                key=button
-            )
-
-
-    def on_scroll_mouse_track(self, x, y, dx, dy):
-        # global is_listen_run
-        # if is_listen_run:
-        #     track_actions.append(f'm{coord_to_string(x, y)}s{dx:+}{dy:+}')
-        if self.is_tracking:
-            self._add_action(
-                'mouse',
-                'scroll',
-                x=x,
-                y=y,
-                dx=dx,
-                dy=dy
-            )
-
-
-    # ^^^^ mouse ^^^^
-    # vvvv keyboard vvvv
-
-    # def key_to_string(key):
-    #     try:
-    #         return key.char
-    #     except AttributeError:
-    #         if key == keyboard.Key.space:
-    #             return space_key_code
-    #         return f'{str(key.value)[1:-1]:0>3}'
-
-    # def string_to_key(string):
-    #     if len(string) > 1:
-    #         if string == space_key_code:
-    #             return keyboard.Key.space
-    #         else:
-    #             return keyboard.KeyCode.from_vk(int(string))
-    #     else:
-    #         return string
-
-
-    pressed_keys = set()
-
-    def on_press_keyboard_track(self, key):
-        # global is_listen_run
-        # if is_listen_run:
-        #     key_pressed = key_to_string(key)
-        #     if key_pressed not in pressed_keys:
-        #         pressed_keys.add(key_pressed)
-        #         track_actions.append(f'kp{key_pressed}')
-        if key == self.hotkey_macros_write:
-            # nothing
-            pass
-        elif self.is_tracking and key not in pressed_keys:
-            self._add_action(
-                'keyboard',
-                'press',
-                key=key
-            )
-            pressed_keys.add(key)
-
-    def on_release_keyboard_track(self, key):
-        # global is_listen_run
-        # if key == hotkey_macros_write:
-        #     is_listen_run = not is_listen_run
-        # elif is_listen_run:
-        #     key_released = key_to_string(key)
-        #     pressed_keys.discard(key_released)
-        #     track_actions.append(f'kr{key_released}')
-        if key == self.hotkey_macros_write:
-            self.is_tracking = not self.is_tracking
-        elif self.is_tracking:
-            self._add_action(
-                'keyboard',
-                'release',
-                key=key
-            )
-            pressed_keys.discard(key)
-
-
-    # ^^^^ keyboard ^^^^
-
-    def start(self):
-        self.keyboard_listener = keyboard.Listener(
-            on_press=self.on_press_keyboard_track,
-            on_release=self.on_release_keyboard_track
-        )
-        self.mouse_listener = mouse.Listener(
-            on_move=self.on_move_mouse_track,
-            on_click=self.on_click_mouse_track,
-            on_scroll=self.on_scroll_mouse_track
-        )
-
-        self.is_listening = True # !!!
-        self.is_tracking = True
-        self.keyboard_listener.start()
-        self.mouse_listener.start()
-
-    def pause(self):
-        self.is_tracking = False
-
-    def unpause(self):
-        self.is_tracking = True
-
-    def stop(self):
-        self.is_listening = False # !!!
-        self.is_tracking = False
-        self.mouse_listener.stop()
-        self.keyboard_listener.stop()
-
-    def run(self):
-        # !!!
-        print(self.track)
-        keyboard_controller = keyboard.Controller()
-        mouse_controller = mouse.Controller()
-
-        for t in self.track:
-            time.sleep(0.2)
-            # Действия клавиатуры
-            if t['device'] == 'keyboard':
-                # Нажатие клавиши клавиатуры
-                if t['action'] == 'press':
-                    keyboard_controller.press(t['key'])
-                # Отпуск клавиши клавиатуры
-                elif t['action'] == 'release':
-                    keyboard_controller.release(t['key'])
-
-            # Действия мыши
-            if t['device'] == 'mouse':
-                # Движение мыши
-                mouse_controller.move(
-                    t['x'] - mouse_controller.position[0],
-                    t['y'] - mouse_controller.position[1]
-                )                
-                # Прокручивание колёсика
-                if t['action'] == 'scroll':
-                    mouse_controller.scroll(t['dx'], t['dy'])
-                # Нажатие клавиши мыши
-                elif t['action'] == 'press':
-                    mouse_controller.press(t['key'])
-                # Отпуск клавиши мыши
-                elif t['action'] == 'release':
-                    mouse_controller.release(t['key'])
-
-    def to_string(self):
-        pass
-
-    def from_string(self, track_str):
-        pass
-
-    def get_action(self, index):
-        pass
-    
+# import time
+# from pynput import keyboard, mouse
+# from screeninfo import get_monitors  
+from actions_track import ActionsTrack  
 
 
 # # Запущена ли прослушка клавиатуры и мыши
@@ -393,7 +152,8 @@ class ActionsTrack:
 
 if __name__ == '__main__':
     '''
-    
+
+
 
     '''
 
@@ -405,9 +165,13 @@ if __name__ == '__main__':
         pass
 
     trck.stop()
+
+    actions = trck.to_string()
+    trck.from_string(actions)
+
     trck.run()
 
-    # # Получение данных о разрешении монитора
+    #   # Получение данных о разрешении монитора
     # for m in get_monitors():
     #     if m.is_primary:
     #         primary_monitor_resolution['width'] = m.width
